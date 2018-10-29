ac040b9c6a66f3dcf1043937bdd2337f293d97fa
Tor Andreas Johnsen <andru_joh@yahoo.no>
2018-10-29 22:16:41 +0100

    Update with es6

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const express = require('express'),
+      mongo = require('mongodb'),
+      mongoose = require('mongoose'),
+      cors = require('cors'),
+      app = express();
+
+// Basic Configuration 
+const port = process.env.PORT || 3000;
+
+/** this project needs a db !! **/ 
+// mongoose.connect(process.env.MONGOLAB_URI);
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${process.cwd()}/views/index.html`);
+});
+
+  
+// your first API endpoint... 
+app.get("/api/hello", (req, res) => {
+  res.json({greeting: 'hello API'});
+});
+
+
+app.listen(port, () => {
+  console.log('Node.js listening ...');
+});
