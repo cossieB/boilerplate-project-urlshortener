c40238c5eead5362fcb0e6d7e87229f56e9d836e
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2020-10-14 12:03:48 +0900

    Update public/style.css
    Co-authored-by: Shaun Hamilton <51722130+Sky020@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+/* This project needs a database */
+// mongoose.connect(process.env.DB_URI);
+
+app.use(cors());
+
+/* This project needs to parse POST bodies */
+// You should mount body-parser here
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
